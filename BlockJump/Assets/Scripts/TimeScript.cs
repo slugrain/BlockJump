ecfda42cec487b7ee933b79f1af59f40ccdbc096
HayashiAyumu <20d3220008ha@edu.tech.ac.jp>
2023-09-09 11:21:52 +0900

    test

--- BlockJump/Assets/Scripts/TimeScript.cs
+++ BlockJump/Assets/Scripts/TimeScript.cs
@@ -14,7 +14,7 @@ public class TimeScript : MonoBehaviour
     private PlayerMove playerMove;
 
     public Text leftTimeText;
-    float leftTime = 10;
+    float leftTime = 60;
 
     void Start()
     {
